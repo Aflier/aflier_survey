6bdf9577e2662fcf242db9b0ce9db69d3b496702
Simon Kiteley <simon@aflier.com>
2021-06-15 15:14:51 +0100

    updates

--- app/helpers/aflier_survey/icons_helper.rb
+++ app/helpers/aflier_survey/icons_helper.rb
@@ -5,14 +5,24 @@ module AflierSurvey
       raw '<i class="fal fa-trash-alt"></i>'
     end
 
+    def icons__up
+      return 'up' if AflierSurvey.configuration.use_words
+      raw '<i class="fal fa-arrow-up"></i>'
+    end
+
+    def icons__down
+      return 'down' if AflierSurvey.configuration.use_words
+      raw '<i class="fal fa-arrow-down"></i>'
+    end
+
     def icons__thumb_down
       return 'thumb down' if AflierSurvey.configuration.use_words
-      raw '<i class="fal fa-thumbs-up"></i>'
+      raw '<i class="fal fa-thumbs-down"></i>'
     end
 
     def icons__thumb_up
       return 'thumb up' if AflierSurvey.configuration.use_words
-      raw '<i class="fal fa-thumbs-down"></i>'
+      raw '<i class="fal fa-thumbs-up"></i>'
     end
 
     def icons__edit
